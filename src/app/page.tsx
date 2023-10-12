3b4b4d243fe869f995742672a9188ce92ed6452c
roxgiba <rox.giba@gmail.com>
2023-10-12 12:42:30 +0200

    top section created and styled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
+"use client";
+
 import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <main>
+      <div className="Logo">
+        <h1>Rick y Morty</h1>
+
+        <form className="search">
+          <input type="search" placeholder="Find a character" />
+        </form>
+      </div>
+      <div className="container">
+        <h3>List of characters</h3>
+        <div></div>
+      </div>
+    </main>
+  );
 }
