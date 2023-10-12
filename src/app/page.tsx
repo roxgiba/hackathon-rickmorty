d9a73f927b5450b42c2e763cc8e52a5b66ef73ca
roxgiba <rox.giba@gmail.com>
2023-10-12 17:54:42 +0200

    h1  updated with a bigger size + letter spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
   return (
     <main>
       <div className="Logo">
-        <h1>Rick y Morty</h1>
+        <h1>Rick & Morty</h1>
 
         <form className="search">
           <input
